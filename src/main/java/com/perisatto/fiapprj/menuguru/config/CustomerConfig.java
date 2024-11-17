cf96f836708f46bdceaaf93393a77aae108e2918
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-11-16 21:54:02 -0300

    Customer service - Minor adjustments

---
+++ src/main/java/com/perisatto/fiapprj/menuguru/config/CustomerConfig.java
@@ -0,0 +1,28 @@
+package com.perisatto.fiapprj.menuguru.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.perisatto.fiapprj.menuguru.infra.gateways.CustomerRepositoyApi;
+import com.perisatto.fiapprj.menuguru.infra.gateways.mappers.CustomerMapper;
+
+@Configuration
+public class CustomerConfig {
+
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	CustomerRepositoyApi customerRepositoyApi(){
+		RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder(); 
+		return new CustomerRepositoyApi(restTemplateBuilder, this.env, this.customerMapper());
+	}
+	
+	@Bean
+	CustomerMapper customerMapper() {
+		return new CustomerMapper();
+	}
+}
